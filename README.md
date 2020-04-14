b217a2ac69b659124138bb061a9d748a8572da69
Chris <chackx91@yahoo.com>
2020-04-14 12:40:11 -0700

    added handlebars and backend

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BurgerApplication
+
+This application utilizes handlebars to modularize the application even further. This follows the MVC(model view controller) paradigm. This application focuses on this practice as it creates moduels for different parts of the project. 
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Chicken Sandwhich', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken Sandwhich', FALSE);
